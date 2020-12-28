057ec534d9996054cae53842ff531c6588c963c8
Jun Yu <shockz@me.com>
2020-12-28 21:33:08 +0900

    라우팅 수정

---
+++ BlazorAcademy/Pages/Portfolios/PortfolioWebSite.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAcademy.Pages.Portfolios
+{
+    public partial class PortfolioWebSite
+    {
+        [Inject]
+        public NavigationManager NavigationManagerRef { get; set; }
+
+        protected override void OnInitialized()
+        {
+
+        }
+
+        protected void GoToDevLog()
+        {
+            //NavigationManagerRef.NavigateTo("https://shockzinfinity.github.io");
+            NavigationManagerRef.NavigateTo("/Index");
+        }
+    }
+}
